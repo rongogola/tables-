543d1eb5c8a269dd50c056e890c576febf317e21
rongogola <ronhoward.gogola.cvt@eac.edu.ph>
2026-09-14 17:08:55 +0800

    Update orders_table.sql

--- orders_table.sql
+++ orders_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE orders (
-    order_id SERIAL PRIMARY KEY,
+    order_id INT PRIMARY KEY,
     customer_id INT,
-    order_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    order_date TIMESTAMP NOT NULL,
     total_amount DECIMAL(10, 2),
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE CASCADE
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
