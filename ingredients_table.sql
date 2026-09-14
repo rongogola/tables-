df78e4f01bd821d889d57a6bbb6fcd6b1c4b7830
rongogola <ronhoward.gogola.cvt@eac.edu.ph>
2026-09-14 17:09:33 +0800

    Update ingredients_table.sql

--- ingredients_table.sql
+++ ingredients_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE ingredients (
-    ingredient_id SERIAL PRIMARY KEY,
+    ingredient_id INT PRIMARY KEY,
     ingredient_name VARCHAR(100) NOT NULL,
     stock_quantity INT NOT NULL
 );
