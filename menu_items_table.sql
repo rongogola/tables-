f525d9e3885fb695d298777c833af431c27d048d
rongogola <ronhoward.gogola.cvt@eac.edu.ph>
2026-09-14 17:08:36 +0800

    Update menu_items_table.sql

--- menu_items_table.sql
+++ menu_items_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE menu_items (
-    item_id SERIAL PRIMARY KEY,
+    item_id INT PRIMARY KEY,
     item_name VARCHAR(100) NOT NULL,
     category VARCHAR(50) NOT NULL,
     price DECIMAL(5, 2) NOT NULL
